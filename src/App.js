3797a3aaedb7da89b58dac8c567008ae05542414
Pavan Kumar S U <104416100+Pavan045-tech@users.noreply.github.com>
2022-08-12 00:27:39 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
 import { Route, Routes } from 'react-router-dom';
-import Add from './Components/3rd-page(Add Word)';
-import Main from './Components/1st-page(Main)';
-import User from './Components/2nd-page(User)';
-import Admin from './Components/2nd-page(Admin)';
+import Add from './3rd-page(Add Word)';
+import Main from './1st-page(Main)';
+import Admin from './2nd-page(Admin)';
+import User from './2nd-page(User)';
 import './App.css';
 
 function App() {
@@ -13,7 +13,8 @@ function App() {
         <Route path="/I'm%20admin" element={<Admin/>}></Route>
         <Route path="/I'm%20a%20User" element={<User/>}></Route>
         <Route path='/Add' element={<Add/>}></Route>
-        <Route path='/2nd' element={<Admin/>}></Route>
+        <Route path='/2nd' element={<Admin/>}></Route> 
+        <Route path="*" element={<Main/>}/>
       </Routes>
   );
 }
